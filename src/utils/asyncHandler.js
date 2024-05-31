99995799f769ef8b3854edc2c51fac6598407559
DEVP-CHANDAN <choubeychandan2463@gmail.com>
2024-05-31 22:11:30 +0530

    register demo

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,9 @@
 const asyncHandler = (asyncHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(asyncHandler(req, res, next)).catch((err) => {
       next(err);
     });
   };
 };
 
-export { asyncHandler };
+export default asyncHandler;
