1b81e88046ed1c1021423931e38118a051570038
DEVP-CHANDAN <choubeychandan2463@gmail.com>
2024-06-05 14:03:13 +0530

    add dashboard

--- src/app.js
+++ src/app.js
@@ -21,6 +21,8 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 //routes declarations
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
@@ -29,4 +31,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 export { app };
